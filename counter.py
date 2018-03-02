7831a7746a5f4835be64c0fea9b6402cdab6ce00
glw <garretlewis@gmail.com>
2018-03-02 15:34:01 -0600

    still trying to trigger camera with pir

--- counter.py
+++ counter.py
@@ -16,15 +16,12 @@ camera.resolution = (400, 300)
 camera.awb_mode = 'auto'
 
 def motion(pir_pin):
-        print("Motion!")
-        filename = datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M')
-        piclocation = '/home/pi/Pictures/'+ filename + '.jpg'
-        time.sleep(1)
-        camera.capture(piclocation)
-        camera.close()
-        file.close()
-        time.sleep(1)
-        return send_file(piclocation)
+    print("Motion!")
+    filename = datetime.strftime(datetime.now(),'%Y-%m-%d-%H-%M-%S')
+    piclocation = '/home/pi/Pictures/'+ filename + '.jpg'
+    time.sleep(1)
+    camera.capture(piclocation)
+    camera.close()
         
 
 print("PIR Module test")
@@ -36,7 +33,28 @@ print("Ready")
 #            print("Motion Detected")
 #        time.sleep(1)
 
-io.remove_event_detect(pir_pin)
-sleep(2)
-io.add_event_detect(pir_pin, io.RISING, callback=motion)
+#io.remove_event_detect(pir_pin)
+#time.sleep(2)
+
+#try:
+#    io.add_event_detect(pir_pin, io.RISING, callback=motion)
+#    while 1:
+#            time.sleep(1)
+#except KeyboardInterrupt:
+#        print "Quit"
+#        io.cleanup()
+#
+
+while True:
+    try:
+        print "rpi-ms-camera: Waiting for motion."
+        io.wait_for_edge(pir_pin, io.RISING)
+        motion(pir_pin)
+        print "rpi-ms-camera: Sleeping "
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print "rpi-ms-camera: Stopping due to keyboard interrupt."
+        camera.close()
+        io.cleanup()
+        break
 
