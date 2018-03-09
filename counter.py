19b853e8e547fcdc2e81ba78a52cd00e0186323d
glw <garretlewis@gmail.com>
2018-03-09 11:07:00 -0600

    PIR and camera finally working together.

--- counter.py
+++ counter.py
@@ -4,7 +4,7 @@ from picamera import PiCamera
 from datetime import datetime
 
 #PIR setup
-pir_pin = 4
+pir_pin = 17
 io.setmode(io.BCM)
 io.setup(pir_pin, io.IN)
 
@@ -21,40 +21,21 @@ def motion(pir_pin):
     piclocation = '/home/pi/Pictures/'+ filename + '.jpg'
     time.sleep(1)
     camera.capture(piclocation)
-    camera.close()
         
 
 print("PIR Module test")
 time.sleep(2)
 print("Ready")
 
-#    while True:
-#        if io.input(pir_pin):
-#            print("Motion Detected")
-#        time.sleep(1)
-
-#io.remove_event_detect(pir_pin)
-#time.sleep(2)
-
-#try:
-#    io.add_event_detect(pir_pin, io.RISING, callback=motion)
-#    while 1:
-#            time.sleep(1)
-#except KeyboardInterrupt:
-#        print "Quit"
-#        io.cleanup()
-#
-
 while True:
     try:
-        print "rpi-ms-camera: Waiting for motion."
+        print ("rpi-ms-camera: Waiting for motion.")
         io.wait_for_edge(pir_pin, io.RISING)
         motion(pir_pin)
-        print "rpi-ms-camera: Sleeping "
+        print ("rpi-ms-camera: Sleeping")
         time.sleep(1)
     except KeyboardInterrupt:
-        print "rpi-ms-camera: Stopping due to keyboard interrupt."
+        print ("rpi-ms-camera: Stopping due to keyboard interrupt.")
         camera.close()
         io.cleanup()
         break
-
